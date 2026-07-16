970b4f48e3adfec141de32a4fd56ce1b99408fff
xwszlz <jiusei0319@gmail.com>
2026-07-17 00:44:26 +0800

    feat: replace homepage banner with V15 (10 machines + AI background + golden ratio layout)

--- src/components/home/recruitment-banner.tsx
+++ src/components/home/recruitment-banner.tsx
@@ -8,7 +8,7 @@ export function RecruitmentBanner({ locale }: RecruitmentBannerProps) {
   return (
     <section className="relative w-full overflow-hidden">
       <img
-        src="/images/banner/banner-chatgpt.jpg"
+        src="/images/banner/banner-v15.webp"
         alt="神雕农机 连接全球二手农机交易"
         className="h-auto w-full object-cover"
       />
